71d0bb6a17b5a88e5f6b7e707627973c16ad14fb
noracyrd <norabyrd@alumni.upenn.edu>
2020-02-07 13:33:36 -0600

    added placeholder button

---
+++ client/src/components/MailingList/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class MailingList extends Component {
+    handleSubmit = () => {
+        console.log('button clicked')
+    }
+    render() {
+        return(
+            <div>
+                <a class="waves-effect waves-light btn-large grey darken-2">JOIN OUR MAILING LIST</a>
+            </div>
+        )
+    }
+}
+
+export default MailingList
