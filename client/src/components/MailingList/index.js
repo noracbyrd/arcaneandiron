33d4ec8c6588193bd2fb402f4e76408c674814f9
noracyrd <norabyrd@alumni.upenn.edu>
2020-03-10 12:17:09 -0500

    removed modal component, worked modal into mailinglist component, basic modal functioning

--- client/src/components/MailingList/index.js
+++ client/src/components/MailingList/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import M from "materialize-css"
 
 class MailingList extends Component {
+    componentDidMount() {
+        M.AutoInit();
+    }
     handleSubmit = () => {
-        console.log('button clicked')
+
     }
     render() {
-        return(
+        return (
             <div>
                 <div className='row'></div>
                 <div className='row'></div>
-                <a className="waves-effect waves-light btn-large grey darken-2" onClick={this.handleSubmit}>JOIN OUR MAILING LIST</a>
+                <a data-target="mailingModal" className="btn modal-trigger waves-effect waves-light btn-large grey darken-2" onClick={this.handleSubmit}>JOIN OUR MAILING LIST</a>
+                <div id="mailingModal" className="modal">
+                    <div className="modal-content">
+                        <h4>Modal Header</h4>
+                        <p>A bunch of text</p>
+                    </div>
+                    <div className="modal-footer">
+                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+                    </div>
+                </div>
             </div>
         )
     }
