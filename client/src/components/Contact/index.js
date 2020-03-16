f6202041f4def413de9b49ad1a4114d765b1d724
noracyrd <norabyrd@alumni.upenn.edu>
2020-03-16 16:48:37 -0500

    replaced class with className in contact component

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -6,29 +6,29 @@ class Contact extends Component {
         return (
             <div id='contact'>
                 <p>OMG ask us all the questionsssssss</p>
-                <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s6">
-                                <input placeholder="Placeholder" id="first_name" type="text" class="validate"></input>
+                <div className="row">
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input placeholder="Placeholder" id="first_name" type="text" className="validate"></input>
                                 <label for="first_name">First Name</label>
                             </div>
-                            <div class="input-field col s6">
-                                <input id="last_name" type="text" class="validate"></input>
+                            <div className="input-field col s6">
+                                <input id="last_name" type="text" className="validate"></input>
                                 <label for="last_name">Last Name</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" type="email" class="validate"></input>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="email" type="email" className="validate"></input>
                                 <label for="email">Email</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <form class="col s12">
-                                <div class="row">
-                                    <div class="input-field col s12">
-                                        <textarea id="textarea1" class="materialize-textarea"></textarea>
+                        <div className="row">
+                            <form className="col s12">
+                                <div className="row">
+                                    <div className="input-field col s12">
+                                        <textarea id="textarea1" className="materialize-textarea"></textarea>
                                         <label for="textarea1">Textarea</label>
                                     </div>
                                 </div>
