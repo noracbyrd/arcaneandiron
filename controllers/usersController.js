0b56cd795326f3ec18417424c72974756fede647
noracyrd <norabyrd@alumni.upenn.edu>
2020-05-16 16:09:26 -0500

    fixed the create function in the usersController and now the database is workinggit add .! yaasssss queen

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,8 +16,8 @@ module.exports = {
     },
     create: function (req, res) {
         console.log('controller is hit')
-        db.User.create(req.body)
         console.log(req.body)
+        db.User.create(req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     }
