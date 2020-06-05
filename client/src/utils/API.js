3e19517cc9a6179b7d73a2c2534723dd72b90e1e
noracyrd <norabyrd@alumni.upenn.edu>
2020-06-05 17:21:23 -0500

    removed console logs from API file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 export default {
     // user routing
     newUser: function (user) {
-        console.log(user)
-        console.log('api is hit wahoowah')
         return axios.post('/api/users', user);
     }
 }
