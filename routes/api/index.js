f07f5c17b988d705c8238aee490b9a1d08c63924
noracyrd <norabyrd@alumni.upenn.edu>
2020-06-11 17:45:07 -0500

    adding new contact routing for contact us emails - added file to api index adn starting to build out the route file with nodemailer

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const emailRoutes = require('./emails.js');
+const contactRoutes = require('./contacts.js')
 
 router.use('/emails', emailRoutes);
+router.use('/contacts', contactRoutes);
 
 module.exports = router;
