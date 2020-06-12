22ae95343190a54674b3c6642c8ea0e7147097a8
noracyrd <norabyrd@alumni.upenn.edu>
2020-06-11 19:17:25 -0500

    added contact function to utils api file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,8 @@ export default {
     // Email routing
     newEmail: function (email) {
         return axios.post('/api/emails', email);
+    },
+    contact: function (contact) {
+        return axios.post('/api/contacts', contact)
     }
 }
